b8e9faf68eb3dd6b7c91b892b9882a9e82ea0a2b
vivek1996 <mvivek3112@gmail.com>
2018-05-30 22:51:22 +0530

    changed getjson to ajax method

--- random_quote/js/main.js
+++ random_quote/js/main.js
@@ -1,19 +1,40 @@
 var msg;
+// function getQuote() {
+//   $.getJSON(
+//     "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
+//     function(data) {
+//       $("#quote").html(data[0].content);
+//       $(".author").html(data[0].title);
+//       msg = data[0].content;
+//       msg = msg.replace(/<p>/g, "");
+//       msg = msg.replace(/<\/p>/g, "");
+//       msg = msg.replace(/ /g, "%20");
+//       var url = `https://twitter.com/intent/tweet?text=${msg}`;
+
+//       $("#twitterUrl").attr("href", url);
+//     }
+//   );
+// }
 function getQuote() {
-  $.getJSON(
-    "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
-    function(data) {
-      $("#quote").html(data[0].content);
-      $(".author").html(data[0].title);
-      msg = data[0].content;
+  $.ajax({
+    url:
+      "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
+    success: function(data) {
+      var post = data.shift();
+      console.log(post);
+      $("#quote").html(post.content);
+      $(".author").html(post.title);
+      msg = post.content;
       msg = msg.replace(/<p>/g, "");
       msg = msg.replace(/<\/p>/g, "");
-      msg = msg.replace(/ /g, "%20");
       var url = `https://twitter.com/intent/tweet?text=${msg}`;
-
       $("#twitterUrl").attr("href", url);
+    },
+    cache: false,
+    error: function(data) {
+      console.log(data);
     }
-  );
+  });
 }
 $(".btn").on("click", function() {
   getQuote();
