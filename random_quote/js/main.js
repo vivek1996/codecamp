bced1158e0375e296027c93da3882a95fa559c73
vivek1996 <mvivek3112@gmail.com>
2018-06-01 20:29:03 +0530

    linted using eslint

--- random_quote/js/main.js
+++ random_quote/js/main.js
@@ -1,56 +1,36 @@
-var msg;
-// function getQuote() {
-//   $.getJSON(
-//     "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
-//     function(data) {
-//       $("#quote").html(data[0].content);
-//       $(".author").html(data[0].title);
-//       msg = data[0].content;
-//       msg = msg.replace(/<p>/g, "");
-//       msg = msg.replace(/<\/p>/g, "");
-//       msg = msg.replace(/ /g, "%20");
-//       var url = `https://twitter.com/intent/tweet?text=${msg}`;
-
-//       $("#twitterUrl").attr("href", url);
-//     }
-//   );
-// }
+let msg;
 function getQuote() {
   $.ajax({
     url:
-      "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
-    success: function(data) {
-      var post = data.shift();
+      'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+    success(data) {
+      const post = data.shift();
       // console.log(post);
-      $("#quote").html(post.content);
-      $(".author").html(post.title);
+      $('#quote').html(post.content);
+      $('.author').html(post.title);
       msg = post.content;
-      msg = msg.replace(/<p>/g, "");
-      msg = msg.replace(/<\/p>/g, "");
-      var url = `https://twitter.com/intent/tweet?text=${msg}`;
-      $("#twitterUrl").attr("href", url);
+      msg = msg.replace(/<p>/g, '');
+      msg = msg.replace(/<\/p>/g, '');
+      const url = `https://twitter.com/intent/tweet?text=${msg}`;
+      $('#twitterUrl').attr('href', url);
     },
     cache: false,
-    error: function(data) {
+    error(data) {
       console.log(data);
-    }
+    },
   });
 }
-$(".btn").on("click", function() {
+$('.btn').on('click', () => {
   getQuote();
 });
 
-$(document).ready(function() {
+$(document).ready(() => {
   Particles.init({
-    selector: ".background",
-    color: ["#08d9d6", "#ff2e63", "#eaeaea"],
+    selector: '.background',
+    color: ['#08d9d6', '#ff2e63', '#eaeaea'],
     sizeVariations: 5,
-    speed: 0.8
+    speed: 0.8,
   });
 
   getQuote();
 });
-
-// $(".fa-twitter").on("click", function() {
-//   window.open(`https://twitter.com/intent/tweet?text=${msg}`, "_blank");
-// });
